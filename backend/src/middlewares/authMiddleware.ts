72c57b0875219b95aafdb90d7d4fe854a7aba17b
Saksham Khare <sakshamkhare19@gmail.com>
2024-11-22 19:29:49 +0530

    middleware to authorize user

---
+++ backend/src/middlewares/authMiddleware.ts
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+import { JWT_SECRET } from "../index.js";
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): any => {
+  const token = req.headers["authorization"]?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({
+      message: `Authentication token is missing`,
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: `Invalid or expired token`,
+    });
+  }
+};
