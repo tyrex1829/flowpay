f0768eb58e78fae65dfb8bc3e74440f0891a4d1f
Saksham Khare <sakshamkhare19@gmail.com>
2024-11-22 19:32:44 +0530

    complete signup, signin, and update profile route for user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,18 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import env from "dotenv";
 env.config();
 import cors from "cors";
 import mongoose from "mongoose";
+import { IUser } from "./types/userTypes.js";
 import { UserModel } from "./models/userModel.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import { authMiddleware } from "./middlewares/authMiddleware.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 const MONGO_URL = process.env.MONGO_URL || "";
-const JWT_SECRET: any = process.env.JWT_SECRET;
+export const JWT_SECRET: any = process.env.JWT_SECRET;
 
 mongoose.connect(MONGO_URL);
 
@@ -28,35 +30,44 @@ connection.on("connected", () => {
 app.use(express.json());
 app.use(cors());
 
-app.post("/new-user", async (req, res): Promise<any> => {
-  const { firstName, lastName, email, password } = req.body;
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the server",
+  });
+});
 
-  if (!firstName || !lastName || !email || !password) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
+app.post(
+  "/new-user",
+  async (req: Request<{}, {}, IUser>, res: Response): Promise<any> => {
+    const { firstName, lastName, email, password } = req.body;
 
-  try {
-    const existingUser = await UserModel.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
+    if (!firstName || !lastName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+      const existingUser = await UserModel.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ message: "User already exists" });
+      }
 
-    const user = new UserModel({
-      firstName,
-      lastName,
-      email,
-      password: hashedPassword,
-    });
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-    await user.save();
+      const user = new UserModel({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+      });
 
-    res.status(200).json({ message: "User created successfully" });
-  } catch (error: any) {
-    res.status(500).json({ error: error.message });
+      await user.save();
+
+      res.status(200).json({ message: "User created successfully" });
+    } catch (error: any) {
+      res.status(500).json({ error: error.message });
+    }
   }
-});
+);
 
 app.post("/login", async (req, res): Promise<any> => {
   const { email, password } = req.body;
@@ -90,6 +101,7 @@ app.post("/login", async (req, res): Promise<any> => {
     const token = jwt.sign(
       {
         email: isUserPresent.email,
+        id: isUserPresent._id,
       },
       JWT_SECRET
     );
@@ -105,10 +117,48 @@ app.post("/login", async (req, res): Promise<any> => {
   }
 });
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Welcome to the server",
-  });
+app.patch("/update-profile", authMiddleware, async (req, res): Promise<any> => {
+  const { firstName, lastName, password } = req.body;
+
+  if (!firstName && !lastName && !password) {
+    return res.status(400).json({
+      message: `At least one field is required to update`,
+    });
+  }
+
+  try {
+    const updateData: {
+      firstName?: string;
+      lastName?: string;
+      password?: string;
+    } = {};
+
+    if (firstName) updateData.firstName = firstName;
+    if (lastName) updateData.lastName = lastName;
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      updateData.password = hashedPassword;
+    }
+
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      req.user?._id,
+      updateData,
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(200).json({
+      message: "Profile updated successfully",
+      user: updatedUser,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Can't update info right now, please try again" });
+  }
 });
 
 app.listen(port, () => {
