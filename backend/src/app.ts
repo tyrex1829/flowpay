8ccb7d4617ce258ccdea624b0c2fba6e053c1b83
Saksham Khare <sakshamkhare19@gmail.com>
2024-11-24 16:16:18 +0530

    complete mvc pattern

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
-import userRoutes from "./routes/userRoutes.js";
+import mainRouter from "./routes/index.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", userRoutes);
+app.use("/api/v1/", mainRouter);
 
 export default app;
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -8,7 +8,7 @@ import {
 
 const router = express.Router();
 
-router.post("/new-user", signupUser);
+router.post("/signup", signupUser);
 
 router.post("/login", signinUser);
 
