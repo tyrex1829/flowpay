6e3b964d469b0e665ba83d17fe21798ad05920e4
Saksham Khare <sakshamkhare19@gmail.com>
2024-11-24 13:27:09 +0530

    getting all values from env file

---
+++ backend/src/config/env.ts
@@ -0,0 +1,13 @@
+import env from "dotenv";
+env.config();
+
+const requiredENV = ["MONGO_URL", "JWT_SECRET", "PORT"];
+requiredENV.forEach((key) => {
+  if (!process.env[key]) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+});
+
+export const MONGO_URL = process.env.MONGO_URL;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const PORT = process.env.PORT || 3000;
