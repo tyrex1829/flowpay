497678e9b931ea639f26c4dfd618c813e6a1695a
Saksham Khare <sakshamkhare19@gmail.com>
2024-11-27 16:37:26 +0530

    add env for development and production

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,7 +1,7 @@
 import env from "dotenv";
 env.config();
 
-const requiredENV = ["MONGO_URL", "JWT_SECRET", "PORT"];
+const requiredENV = ["MONGO_URL", "JWT_SECRET", "PORT", "NODE_ENV"];
 requiredENV.forEach((key) => {
   if (!process.env[key]) {
     throw new Error(`Missing environment variable: ${key}`);
@@ -11,3 +11,4 @@ requiredENV.forEach((key) => {
 export const MONGO_URL = process.env.MONGO_URL!;
 export const JWT_SECRET = process.env.JWT_SECRET!;
 export const PORT = process.env.PORT || 3000;
+export const NODE_ENV = process.env.NODE_ENV!;
