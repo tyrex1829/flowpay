a3797df63262338d85fc9e96b562cd80122d8369
Saksham Khare <sakshamkhare19@gmail.com>
2024-11-27 16:06:17 +0530

    Fix: User model-scehma

--- backend/src/types/userTypes.ts
+++ backend/src/types/userTypes.ts
@@ -1,8 +1,7 @@
 import { Document } from "mongoose";
 
 export interface IUser extends Document {
-  firstName: string;
-  lastName: string;
   email: string;
   password: string;
+  username: string;
 }
