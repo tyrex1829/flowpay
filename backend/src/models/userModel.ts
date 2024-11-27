c61c823675996d6673117f29dc2cc24aa4afeae9
Saksham Khare <sakshamkhare19@gmail.com>
2024-11-27 16:40:21 +0530

    Feat: user-model, video-model

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -1,32 +1,16 @@
 import mongoose, { ObjectId } from "mongoose";
 import { IUser } from "../types/userTypes.js";
-import { Account } from "../types/accountTypes.js";
 
 const UserSchema = new mongoose.Schema<IUser>(
   {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    username: { type: String, required: true },
   },
   { timestamps: true }
 );
-
-const AccountSchema = new mongoose.Schema<Account>({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  balance: {
-    type: Number,
-    required: true,
-  },
-});
 
 export const UserModel = mongoose.model<IUser & mongoose.Document>(
   "User",
   UserSchema
 );
-
-export const AccountModel = mongoose.model("Account", AccountSchema);
---
+++ backend/src/models/videoModel.ts
@@ -0,0 +1,20 @@
+import mongoose, { ObjectId } from "mongoose";
+import { Video } from "../types/videoTypes.js";
+
+const VideoSchema = new mongoose.Schema<Video>(
+  {
+    title: { type: String, required: true },
+    thumbnail_url: { type: String, required: true },
+    creator: {
+      id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+      username: { type: String, required: true },
+    },
+    view_count: { type: Number, default: 0, required: true },
+  },
+  { timestamps: true }
+);
+
+export const VideoModel = mongoose.model<Video & mongoose.Document>(
+  "Video",
+  VideoSchema
+);
